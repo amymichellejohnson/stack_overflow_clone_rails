ed8bd9dfb3a92bded1dd2fdb2c768d84b4e100a3
Alex Beeken and Amy Johnson <amyjohnsonmichelle@gmail.com>
2015-03-23 11:31:02 -0700

    Have User Model and Table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150323163049) do
+ActiveRecord::Schema.define(version: 20150323182913) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,14 @@ ActiveRecord::Schema.define(version: 20150323163049) do
 
   create_table "questions", force: :cascade do |t|
     t.string   "body"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "email"
+    t.string   "password_hash"
+    t.string   "password_salt"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
