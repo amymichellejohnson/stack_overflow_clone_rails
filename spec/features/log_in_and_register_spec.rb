3c05bc05f44e164d08c36e6c86f4af7f3e557b33
Alex Beeken and Amy Johnson <amyjohnsonmichelle@gmail.com>
2015-03-24 10:29:07 -0700

    Hide Login and New User Options When Logged In

--- spec/features/log_in_and_register_spec.rb
+++ spec/features/log_in_and_register_spec.rb
@@ -21,15 +21,21 @@ describe "the registration process" do
     expect(page).to have_content "Successfully"
   end
 
-  # it 'hides the New User and Log in options when logged in' do
-  #   user = FactoryGirl.create(:user)
-  #   visit questions_path
-  #   click_on 'Log in'
-  #   fill_in 'Email', :with => user.email
-  #   fill_in 'Password', :with => user.password
-  #   click_on 'Log in'
-  #   visit questions_path
-  #   click_on 'Log in'
-  #   expect(page).to have_content "Successfully"
-  # end
+  it 'hides the New User and Log in options when logged in' do
+    user = FactoryGirl.create(:user)
+    visit questions_path
+    click_on 'Log in'
+    fill_in 'Email', :with => user.email
+    fill_in 'Password', :with => user.password
+    click_on 'Log in'
+    expect(page).not_to have_content "Log in"
+    expect(page).not_to have_content "New User"
+  end
+
+  it 'hides the New User and Log in options when logged in' do
+    visit questions_path
+    expect(page).not_to have_content "Ask a Question"
+    expect(page).not_to have_content "Answer"
+    expect(page).not_to have_content "Log Out"
+  end
 end
