cc746f7c37937f6221ea3d0d2d670fe80adb5ea7
Alex Beeken and Amy Johnson <amyjohnsonmichelle@gmail.com>
2015-03-24 16:25:33 -0700

    Sort Answers By Vote Amount

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -2,6 +2,9 @@ class VotesController < ApplicationController
 
   def create
     Vote.create({:user_id => current_user.id, :answer_id => params[:answer_id]})
+    answer = Answer.find(params[:answer_id])
+    count = answer.count + 1
+    answer.update({:count => count})
     flash[:notice] = "That was successfully submitted!"
     redirect_to questions_path
   end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150324211749) do
+ActiveRecord::Schema.define(version: 20150324231757) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 20150324211749) do
     t.datetime "updated_at"
     t.integer  "user_id"
     t.boolean  "is_first",    default: false
+    t.integer  "count",       default: 0
   end
 
   create_table "questions", force: :cascade do |t|
